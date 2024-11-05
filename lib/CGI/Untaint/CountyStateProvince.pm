7f0b597494331e8f6931ad0338f37bd3efc73860
Nigel Horne <njh@bandsman.co.uk>
2024-11-05 08:41:33 -0500

    Use gtar to create a distribution on Macs

--- lib/CGI/Untaint/CountyStateProvince.pm
+++ lib/CGI/Untaint/CountyStateProvince.pm
@@ -70,20 +70,18 @@ sub _untaint_re {
 sub is_valid {
 	my $self = shift;
 
-	unless(scalar(@countries) > 0) {
-		carp "You must specify at least one country";
+	unless(@countries) {
+		carp 'You must specify at least one country';
 		return 0;
 	}
 
-	my $value = $self->value;
+	my $value = $self->value();
 
-	foreach my $country (@countries) {
+	foreach my $country(@countries) {
 		$country->value($value);
-		my $new_value = $country->is_valid();
-		if($new_value) {
-			if($new_value ne $value) {
-				$self->value($new_value);
-			}
+
+		if(my $new_value = $country->is_valid()) {
+			$self->value($new_value) if $new_value ne $value;
 		} else {
 			return 0;
 		}
@@ -91,6 +89,7 @@ sub is_valid {
 
 	return 1;
 }
+
 
 =head1 AUTHOR
 
@@ -101,7 +100,6 @@ Nigel Horne, C<< <njh at bandsman.co.uk> >>
 Please report any bugs or feature requests to C<bug-cgi-untaint-countystateprovince at rt.cpan.org>, or through
 the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=CGI-Untaint-CountyStateProvince>.  I will be notified, and then you'll
 automatically be notified of progress on your bug as I make changes.
-
 
 =head1 SEE ALSO
 
@@ -114,7 +112,6 @@ You can find documentation for this module with the perldoc command.
 
     perldoc CGI::Untaint::CountyStateProvince
 
-
 You can also look for information at:
 
 =over 4
@@ -123,23 +120,18 @@ You can also look for information at:
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=CGI-Untaint-CountyStateProvince>
 
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/CGI-Untaint-CountyStateProvince>
-
 =item * Search CPAN
 
 L<http://search.cpan.org/dist/CGI-Untaint-CountyStateProvince>
 
 =back
 
-
 =head1 ACKNOWLEDGEMENTS
 
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2012-2019 Nigel Horne.
+Copyright 2012-2024 Nigel Horne.
 
 This program is released under the following licence: GPL2
 
